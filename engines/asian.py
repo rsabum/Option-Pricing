3a8268ed473b278f29c9a0306372786bfef44d3e
robertsabum <rjsabum@noctrl.edu>
2024-08-08 20:50:57 -0500

    updated flags to boolean values and completed barrier options algorithms

--- engines/asian.py
+++ engines/asian.py
@@ -9,19 +9,16 @@ def Price_Asian_Option(
     period: int,
     num_simulations: int,
     num_timesteps: int,
-    option_style: str,
-    averaging_style: str,
-    exercise_style: str
+    call_option: bool = True,
+    arithmetic_averaging: bool = True,
+    european_exercise: bool = True
 ):
     
-    if option_style == "call":
+    if call_option:
         exercise_value = lambda mean: max(mean - strike, 0)
 
-    elif option_style == "put":
-        exercise_value = lambda mean: max(strike - mean, 0)
-
     else:
-        raise Exception("Invalid option type. Expected \"call\" or \"put\"")
+        exercise_value = lambda mean: max(strike - mean, 0)
     
 
     PRICE = asset_model.simulate(
@@ -31,41 +28,34 @@ def Price_Asian_Option(
         N=num_timesteps
     )
 
-    # calculating ruinning mean of price
+    # calculating ruinning average of the asset's price
     MEAN = np.zeros(shape=(num_simulations, num_timesteps + 1))
     MEAN[:, 0] = PRICE[:, 0]
 
-    if averaging_style == "geometric":
+    if arithmetic_averaging:
+        for t in range(1, num_timesteps + 1):
+            MEAN[:, t] = (MEAN[:, t - 1] * t + PRICE[:, t]) / (t + 1)
+    
+    else:
         for t in range(1, num_timesteps + 1):
             MEAN[:, t] = (PRICE[:, t] * MEAN[:, t - 1] ** t) ** (1 / (t + 1))
 
-    elif averaging_style == "arithmetic":
-        for t in range(1, num_timesteps + 1):
-            MEAN[:, t] = (MEAN[:, t - 1] * t + PRICE[:, t]) / (t + 1)
 
-    else:
-        raise Exception("Invalid Asian Option Averaging Style. Expected \"arithmetic\" or \"geometric\"")
-
-
-    if exercise_style == "european":
-        G = np.zeros(shape=(num_simulations))
+    if european_exercise:
+        VALUE = np.zeros(shape=(num_simulations))
         for i in range(num_simulations):
-            G[i] = exercise_value(MEAN[i][-1])
+            VALUE[i] = exercise_value(MEAN[i][-1])
 
-        return np.mean(G)
+        return np.mean(VALUE)
     
-
-    if exercise_style == "american":
-        G = np.zeros(shape=(num_simulations, num_timesteps + 1))
+    else:
+        VALUE = np.zeros(shape=(num_simulations, num_timesteps + 1))
         for i in range(num_simulations):
             for t in reversed(range(num_timesteps + 1)):
                 if t == num_timesteps:
-                    G[i][t] = exercise_value(MEAN[i][t])
+                    VALUE[i][t] = exercise_value(MEAN[i][t])
                 
                 else:
-                    G[i][t] = max(exercise_value(MEAN[i][t]), G[i][t + 1])
+                    VALUE[i][t] = max(exercise_value(MEAN[i][t]), VALUE[i][t + 1])
 
-        return np.mean(G[:, 0])
-    
-    
-    raise Exception("Invalid Option Exercise Style. Expected \"european\" or \"american\"")
+        return np.mean(VALUE[:, 0])
---
+++ engines/barrier.py
@@ -0,0 +1,78 @@
+from models import model
+import numpy as np
+
+
+def Price_Barrier_Option(
+    asset_model: model,
+    asset_price: float,
+    barrier: float,
+    strike: float,
+    period: int,
+    num_simulations: int,
+    num_timesteps: int,
+    call_option: bool = True,
+    knock_in: bool = True,
+    barrier_up: bool = True,
+    european_exercise: bool = True
+):
+    
+    if knock_in:
+        if call_option:
+            exercise_value = lambda hit, price: max(price - strike, 0) if hit else 0
+
+        else:
+            exercise_value = lambda hit, price: max(strike - price, 0) if hit else 0
+
+    else:
+        if call_option:
+            exercise_value = lambda hit, price: 0 if hit else max(price - strike, 0)
+
+        else:
+            exercise_value = lambda hit, price: 0 if hit else max(strike - price, 0)
+    
+
+    PRICE = asset_model.simulate(
+        S0=asset_price, 
+        T=period, 
+        M=num_simulations, 
+        N=num_timesteps
+    )
+
+    # calculating running maximum of the asset's price
+    CROSSED = np.zeros(shape=(num_simulations, num_timesteps + 1))
+
+    if barrier_up:
+        for i in range(num_simulations):
+            CROSSED[i][0] = PRICE[i][0] >= barrier
+            for t in range(1, num_timesteps + 1):
+                CROSSED[i, t] = max(CROSSED[i][t - 1], PRICE[i][t] >= barrier)
+
+    else:
+        for i in range(num_simulations):
+            CROSSED[i][0] = PRICE[i][0] <= barrier
+            for t in range(1, num_timesteps + 1):
+                CROSSED[i, t] = max(CROSSED[i][t - 1], PRICE[i][t] <= barrier)
+
+
+    if european_exercise:
+        VALUE = np.zeros(shape=(num_simulations))
+        for i in range(num_simulations):
+            VALUE[i] = exercise_value(CROSSED[i][-1], PRICE[i][-1])
+
+        return np.mean(VALUE)
+    
+
+    else:
+        VALUE = np.zeros(shape=(num_simulations, num_timesteps + 1))
+        for i in range(num_simulations):
+            for t in reversed(range(num_timesteps + 1)):
+                if t == num_timesteps:
+                    VALUE[i][t] = exercise_value(CROSSED[i][t], PRICE[i][t])
+                
+                else:
+                    VALUE[i][t] = max(
+                        exercise_value(CROSSED[i][t], PRICE[i][t]), 
+                        VALUE[i][t + 1]
+                    )
+
+        return np.mean(VALUE[:, 0])
